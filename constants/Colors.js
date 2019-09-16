0517df46f09ba140aad6ab29e9855a4b77de98b1
Kim Graff <kimagraff@gmail.com>
2019-09-16 09:45:37 -0400

    change icon colors

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,10 +1,11 @@
-const tintColor = '#ccc';
+const tintColor = '#8e2138';
 
 export default {
   tintColor,
-  tabIconDefault: '#ccc',
+  tabIconDefault: '#3a121a',
   tabIconSelected: tintColor,
   tabBar: '#3a121a',
+  color: '#3a121a',
   errorBackground: 'red',
   errorText: '#fff',
   warningBackground: '#EAEB5E',
