b36570a39556583a57447ff78ce6ef76026f4aed
keenan560 <keenan560@gmail.com>
2019-09-21 20:56:56 -0400

    no changes

--- App.js
+++ App.js
@@ -17,13 +17,13 @@ export default function App(props) {
 
   useEffect(() => {
     var firebaseConfig = {
-      apiKey: "AIzaSyAHhnWeyBtUHJTtigUNMwQv5naDfNwqoOQ",
-      authDomain: "musability-91b3d.firebaseapp.com",
-      databaseURL: "https://musability-91b3d.firebaseio.com",
-      projectId: "musability-91b3d",
+      apiKey: "AIzaSyBF2aWOLg8IYO9ntBNk6agDXdrasaQMwkM",
+      authDomain: "musability-app.firebaseapp.com",
+      databaseURL: "https://musability-app.firebaseio.com",
+      projectId: "musability-app",
       storageBucket: "",
-      messagingSenderId: "168169604472",
-      appId: "1:168169604472:web:32bccbafe468799ff2b48d"
+      messagingSenderId: "93508034987",
+      appId: "1:93508034987:web:454f410ea139fe4c2932ee"
     };
 
     // // Initialize Firebase
@@ -65,23 +65,23 @@ export default function App(props) {
 
     //Code to initially load all the data from venue.JSON into Firbase, needed only to run once.
 
-    // VENUES.forEach(element => {
+    VENUES.forEach(element => {
 
-    //   var venue = {
-    //     name: element.venue ? element.venue: "",
-    //     website: element.href ? element.href: "",
-    //     address: element.address ? element.address: "",
-    //     overallRating: element.overallRating ? element.overallRating: "",
-    //     anonymityRating: element.anonymityRating ? element.anonymityRating: "",
-    //     elevator: element.elevator ? element.elevator: null,
-    //     ramps: element.ramps ? element.ramps: null,
-    //     rampComment: element.rampComment ? element.rampComment: "",
-    //     restrooms: element.restrooms ? element.restrooms: null,
-    //     restroomKey: element.restroomKey ? element.restroomKey: null,
-    //     restroomsComment: element.restroomsComment ? element.restroomsComment: "",
-    //     overallComment: element.overallComment ? element.overallRating: "",
-    //     image: element.image.length !== 0 ? element.image: ""
-    //   }
+      var venue = {
+        name: element.venue ? element.venue: "",
+        website: element.href ? element.href: "",
+        address: element.address ? element.address: "",
+        overallRating: element.overallRating ? element.overallRating: "",
+        anonymityRating: element.anonymityRating ? element.anonymityRating: "",
+        elevator: element.elevator ? element.elevator: null,
+        ramps: element.ramps ? element.ramps: null,
+        rampComment: element.rampComment ? element.rampComment: "",
+        restrooms: element.restrooms ? element.restrooms: null,
+        restroomKey: element.restroomKey ? element.restroomKey: null,
+        restroomsComment: element.restroomsComment ? element.restroomsComment: "",
+        overallComment: element.overallComment ? element.overallRating: "",
+        image: element.image.length !== 0 ? element.image: ""
+      }
 
 
     // // database.ref("/venues").push(venue);
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -32,102 +32,99 @@ const MUSIC_IMAGE = require('../assets/images/musicnote.png');
 
 
 export default class HomeScreen extends Component {
-  _renderItem ({item, index}) {
-     
   
-
-  constructor(props) {
-    super(props);
-    this.state = {
+    constructor(props) {
+      super(props);
+      this.state = {
         slider1ActiveSlide: SLIDER_1_FIRST_ITEM,
         visibleModalId: null,
-    };
-  }
+      };
+    }
 
-  componentDidMount() {
-    const data = VENUES;
-    this.setState({ venues: data });
-  }
-
-
-
-_renderItem ({item, index}) {
-    return <MyCarousel data={item} even={(index + 1) % 2 === 0} onPress={visibleModal}/>;
-}
+    componentDidMount() {
+      const data = VENUES;
+      this.setState({ venues: data });
+    }
 
 
-_renderItemWithParallax ({item, index}, parallaxProps) {
-    return (
-      <MyCarousel
-        data={item}
-        even={(index + 1) % 2 === 0}
-        parallax={true}
-        parallaxProps={parallaxProps}
-      />
-    );
 
-  }
+    _renderItem({ item, index }) {
+      return <MyCarousel data={item} even={(index + 1) % 2 === 0} onPress={visibleModal} />;
+    }
 
-  _renderLightItem({ item, index }) {
-    // console.log("TCL: _renderLightItem -> item", item)
-  
-    return <MyCarousel data={item} even={false} />;
-  }
 
-  _renderDarkItem({ item, index }) {
-    return <MyCarousel data={item} even={true} />;
-  }
+    _renderItemWithParallax({ item, index }, parallaxProps) {
+      return (
+        <MyCarousel
+          data={item}
+          even={(index + 1) % 2 === 0}
+          parallax={true}
+          parallaxProps={parallaxProps}
+        />
+      );
 
-// VENUE CARD INFORMATION 
-  venueCard (number, title, type) {
-    const isTinder = type === 'tinder';
+    }
+
+    _renderLightItem({ item, index }) {
+      // console.log("TCL: _renderLightItem -> item", item)
+
+      return <MyCarousel data={item} even={false} />;
+    }
+
+    _renderDarkItem({ item, index }) {
+      return <MyCarousel data={item} even={true} />;
+    }
+
+    // VENUE CARD INFORMATION 
+    venueCard(number, title, type) {
+      const isTinder = type === 'tinder';
       return (
         <View style={[styles.exampleContainer, isTinder ? styles.exampleContainerDark : styles.exampleContainerLight]}>
-          <Text style={[styles.title, isTinder ? {} : styles.titleDark]}  > 
+          <Text style={[styles.title, isTinder ? {} : styles.titleDark]}  >
             {`Brooklyn`}
           </Text>
           <Text style={[styles.subtitle, isTinder ? {} : styles.titleDark]}>
           </Text>
-            <Carousel
-                  data={VENUES}
-                  renderItem={isTinder ? this._renderLightItem : this._renderItem}
-                  sliderWidth={sliderWidth}
-                  itemWidth={itemWidth}
-                  containerCustomStyle={styles.slider}
-                  contentContainerCustomStyle={styles.sliderContentContainer}
-                  layout={type}
-                  loop={true}
-                  onPress={visibleModal}
-            />
+          <Carousel
+            data={VENUES}
+            renderItem={isTinder ? this._renderLightItem : this._renderItem}
+            sliderWidth={sliderWidth}
+            itemWidth={itemWidth}
+            containerCustomStyle={styles.slider}
+            contentContainerCustomStyle={styles.sliderContentContainer}
+            layout={type}
+            loop={true}
+            onPress={visibleModal}
+          />
         </View>
-        );
+      );
     };
 
 
-    venueCard2 (number, title, type) {
+    venueCard2(number, title, type) {
       const isTinder = type === 'tinder';
-        return (
-          <View style={[styles.exampleContainer, isTinder ? styles.exampleContainerDark : styles.exampleContainerLight]}>
-            <Text style={[styles.title, isTinder ? {} : styles.titleDark]}> 
-              {`Manhattan`}
-            </Text>
-            <Text style={[styles.subtitle, isTinder ? {} : styles.titleDark]}>
-            </Text>
-              <Carousel
-                    data={VENUES}
-                    renderItem={isTinder ? this._renderLightItem : this._renderItem}
-                    sliderWidth={sliderWidth}
-                    itemWidth={itemWidth}
-                    containerCustomStyle={styles.slider}
-                    contentContainerCustomStyle={styles.sliderContentContainer}
-                    layout={type}
-                    loop={true}
-                    onPress={visibleModal}
-              />
-          </View>
-          );
-      };
-  
+      return (
+        <View style={[styles.exampleContainer, isTinder ? styles.exampleContainerDark : styles.exampleContainerLight]}>
+          <Text style={[styles.title, isTinder ? {} : styles.titleDark]}>
+            {`Manhattan`}
+          </Text>
+          <Text style={[styles.subtitle, isTinder ? {} : styles.titleDark]}>
+          </Text>
+          <Carousel
+            data={VENUES}
+            renderItem={isTinder ? this._renderLightItem : this._renderItem}
+            sliderWidth={sliderWidth}
+            itemWidth={itemWidth}
+            containerCustomStyle={styles.slider}
+            contentContainerCustomStyle={styles.sliderContentContainer}
+            layout={type}
+            loop={true}
+            onPress={visibleModal}
+          />
+        </View>
+      );
+    };
+
 
     const renderCard = this.venueCard(3, '"Stack of cards" layout | Loop', 'stack');
     const renderCard2 = this.venueCard2(3, '"Stack of cards" layout | Loop', 'stack');
@@ -156,44 +153,44 @@ _renderItemWithParallax ({item, index}, parallaxProps) {
               style={styles.welcomeImage}
             />
 
-      {/* Search Bar */}
-      <Grid>
-        <Row>
-          <TextInput
-            // onChangeText={text => onChangeText(text)}
-            // value={value}
-              style={{ height: 40, borderColor: 'white', borderRadius: 10, borderWidth: 1, backgroundColor: 'white', width: 300, marginLeft: 20 }}
-          />
-          <Button
-              buttonStyle={{ borderRadius: 10, marginLeft: 5, marginRight: 0, marginBottom: 0, backgroundColor: '#000000' }}
-              title='Search'/>
-        </Row>
+            {/* Search Bar */}
+            <Grid>
+              <Row>
+                <TextInput
+                  // onChangeText={text => onChangeText(text)}
+                  // value={value}
+                  style={{ height: 40, borderColor: 'white', borderRadius: 10, borderWidth: 1, backgroundColor: 'white', width: 300, marginLeft: 20 }}
+                />
+                <Button
+                  buttonStyle={{ borderRadius: 10, marginLeft: 5, marginRight: 0, marginBottom: 0, backgroundColor: '#000000' }}
+                  title='Search' />
+              </Row>
 
-        <Row>
-          <SafeAreaView style={styles.safeArea}>
-            <View>
-              <StatusBar
-                translucent={true}
-                backgroundColor={'rgba(58, 18, 26, .8)'}
-                barStyle={'light-content'}
-              />
-                 { this.gradient }
-              <ScrollView
-                style={styles.scrollview}
-                scrollEventThrottle={200}
-                directionalLockEnabled={true}
-              >
-                { renderCard }
-                { renderCard2 }
-              </ScrollView>
-            </View>
-          </SafeAreaView>
-        </Row>
+              <Row>
+                <SafeAreaView style={styles.safeArea}>
+                  <View>
+                    <StatusBar
+                      translucent={true}
+                      backgroundColor={'rgba(58, 18, 26, .8)'}
+                      barStyle={'light-content'}
+                    />
+                    {this.gradient}
+                    <ScrollView
+                      style={styles.scrollview}
+                      scrollEventThrottle={200}
+                      directionalLockEnabled={true}
+                    >
+                      {renderCard}
+                      {renderCard2}
+                    </ScrollView>
+                  </View>
+                </SafeAreaView>
+              </Row>
 
-      </ Grid>
-    </View>
-  </ScrollView>
-</View>
-  );  
+            </ Grid>
+          </View>
+        </ScrollView>
+      </View>
+    );
   }
 };
