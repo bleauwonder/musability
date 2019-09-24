44d77e5714fc81f7982c19885013126da6d2c4aa
bleauwonder <elyzableau.om@gmail.com>
2019-09-23 22:26:42 -0400

    adjusted last commit

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ import VENUES from './components/venueJSON';
 import AppNavigator from './navigation/AppNavigator';
 import { Container, Form, Input, Button, Label } from "native-base";
 import LogIn from './components/LogIn';
-import { createRootNavigator } from "./navigation/router";
+// import { createRootNavigator } from "./navigation/router";
 import { isSignedIn } from "./auth";
 // export default class App extends React.Component {
 //   constructor(props) {
@@ -151,31 +151,6 @@ function handleLoadingError(error) {
 }
 function handleFinishLoading(setLoadingComplete) {
   setLoadingComplete(true);
-} };
-}
+} 
 
-async function loadResourcesAsync() {
-  await Promise.all([
-    Asset.loadAsync([
-      require('./assets/images/robot-dev.png'),
-      require('./assets/images/robot-prod.png'),
-    ]),
-    Font.loadAsync({
-      // This is the font that we are using for our tab bar
-      ...Ionicons.font,
-      // We include SpaceMono because we use it in HomeScreen.js. Feel free to
-      // remove this if you are not using it in your app
-      'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-    }),
-  ]);
-}
 
-function handleLoadingError(error) {
-  // In this case, you might want to report the error to your error reporting
-  // service, for example Sentry
-  console.warn(error);
-}
-
-function handleFinishLoading(setLoadingComplete) {
-  setLoadingComplete(true);
-}
