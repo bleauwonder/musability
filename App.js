77f8b2939ce7f00788f7a20b50fd9c056a4cc16f
keenan560 <keenan560@gmail.com>
2019-09-23 22:28:23 -0400

    fixed dups

--- App.js
+++ App.js
@@ -145,37 +145,10 @@ async function loadResourcesAsync() {
   ]);
 }
 function handleLoadingError(error) {
-  // In this case, you might want to report the error to your error reporting
-  // service, for example Sentry
+ 
   console.warn(error);
 }
 function handleFinishLoading(setLoadingComplete) {
   setLoadingComplete(true);
-} };
-}
+} 
 
-async function loadResourcesAsync() {
-  await Promise.all([
-    Asset.loadAsync([
-      require('./assets/images/robot-dev.png'),
-      require('./assets/images/robot-prod.png'),
-    ]),
-    Font.loadAsync({
-      // This is the font that we are using for our tab bar
-      ...Ionicons.font,
-      // We include SpaceMono because we use it in HomeScreen.js. Feel free to
-      // remove this if you are not using it in your app
-      'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-    }),
-  ]);
-}
-
-function handleLoadingError(error) {
-  // In this case, you might want to report the error to your error reporting
-  // service, for example Sentry
-  console.warn(error);
-}
-
-function handleFinishLoading(setLoadingComplete) {
-  setLoadingComplete(true);
-}
