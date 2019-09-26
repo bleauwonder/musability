fd2de2a3a9a93af3ecfefacf1573462e8d7821b3
Kim Graff <kimagraff@gmail.com>
2019-09-26 17:46:01 -0400

    login function redirections to home page now

--- screens/LogInScreen.js
+++ screens/LogInScreen.js
@@ -1,4 +1,3 @@
-
 import * as WebBrowser from 'expo-web-browser';
 import React, { Component } from 'react';
 import {
@@ -9,26 +8,133 @@ import {
   SafeAreaView,
   Text,
   TouchableOpacity,
+  ImageBackground,
   View,
   Dimensions,
-  ImageBackground,
-  TextInput
+  TextInput,
 } from 'react-native';
-import LogIn from '../components/LogIn';
 import { LinearGradient } from 'expo-linear-gradient';
-import styles, { colors } from '../src/style/index.style'
+import loginImage from '../assets/images/login.jpg';
+import styles, { colors } from '../src/style/index.style';
 import logo from '../assets/images/muslogo.png';
 import * as firebase from 'firebase';
 const IS_ANDROID = Platform.OS === 'android';
-import loginImage from '../assets/images/login.jpg'
 import { Container, Item, Form, Input, Button, Label } from "native-base";
+// const { navigate } = this.props.navigation;
+const firebaseConfig = {
+  apiKey: "AIzaSyBF2aWOLg8IYO9ntBNk6agDXdrasaQMwkM",
+  authDomain: "musability-app.firebaseapp.com",
+  databaseURL: "https://musability-app.firebaseio.com",
+  projectId: "musability-app",
+  storageBucket: "musability-app.appspot.com",
+  messagingSenderId: "93508034987",
+  appId: "1:93508034987:web:454f410ea139fe4c2932ee"
+};
+firebase.initializeApp(firebaseConfig);
 
 export default class LogInScreen extends React.Component {
-render () {
-    //FIRST SHOWS - LOG IN PAGE / SIGN UP PAGE
-    //CHANGE TO COPOMENT LATER 
-      return (   
-        <LogIn />
-      );
+//props and states related to singin function
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+
+//creating sign up function
+  SignUp = (email, password) => {
+    try {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then(() => this.props.navigation.navigate('Home'))
+    } catch (error) {
+      console.log(error.toString(error));
     }
+  };
+
+//creating log in function
+  LogIn = (email, password) => {
+    try {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .then(() => this.props.navigation.navigate('Home'))
+        // .then(res => {
+        //   console.log(res.user.email);
+        //         });
+    } catch (error) {
+      console.log(error.toString(error));
+    }
+  };
+
+  //RENDERING PAGE VIEW BELOW 
+  render() {
+    return (
+        <View style={styles.loginContainer}>
+         <LinearGradient
+              colors={['rgba(142, 33, 56, 100)', 'rgba(121, 9, 9, 100)', 'transparent']}
+              style={{
+                position: 'absolute',
+                left: 0,
+                right: 0,
+                top: 0,
+                height: 850,
+              }}
+            />
+            <Image
+              source={
+                __DEV__
+                  ? require('../assets/images/muslogo.png')
+                  : require('../assets/images/muslogo.png')
+              }
+              style={styles.logImage}
+            />
+          <Container style={styles.logIn}>
+            <Form>
+              <Item floatingLabel>
+              <Label style={{color: 'white'}}>Email</Label>
+                <Input
+                  style={styles.styleInput}
+                  placeholder='you@domain.com'
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  onChangeText={email => this.setState({ email })}
+                />
+              </Item>
+              <Item floatingLabel>
+                <Label style={{color: 'white'}}>Password</Label>
+                <Input
+                  style={styles.styleInput}
+                  color='white'
+                  secureTextEntry={true}
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  onChangeText={password => this.setState({ password })}
+                />
+              </Item>
+
+              <Button
+                full rounded
+                light
+                style={styles.logButton}
+                onPress={() => this.LogIn(this.state.email, this.state.password)
+                }>
+                <Text style={[styles.buttonText, {color: 'black'}]}>LOGIN</Text>
+              </Button>
+
+              <Button
+                full rounded dark
+                style={styles.logButton}
+                onPress={() => this.SignUp(this.state.email, this.state.password)}>
+                <Text style={styles.buttonText}>SIGNUP</Text>
+              </Button>
+            </Form>
+          </ Container>
+        </View>
+    );
+  }
 }
+
